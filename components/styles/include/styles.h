5a0a5e91de5b2fe200c4dae09a126854b74a1319
Dragos Hatagan <dragos.hategan@yahoo.com>
2025-12-06 11:24:27 +0200

    Added styles component and font Domine_16

---
+++ components/styles/include/styles.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+
+#define UI_COLOR_BG             lv_color_hex(0x101214)
+#define UI_COLOR_CARD           lv_color_hex(0x202327)
+#define UI_COLOR_BORDER         lv_color_hex(0x2D3034)
+#define UI_COLOR_TEXT           lv_color_hex(0xDCDCDC)
+#define UI_COLOR_ACCENT_BLUE    lv_color_hex(0x7D5FFF)
+#define UI_COLOR_ACCENT_BLUE_2  lv_color_hex(0x347AFF)
+#define UI_COLOR_ACCENT_GREEN   lv_color_hex(0x37B24D)
+
+
+void styles_build_msgbox(lv_obj_t *mbox);
+void styles_build_keyboard(lv_obj_t *kbd);
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ components/styles/styles.c
@@ -0,0 +1,45 @@
+#include "styles.h"
+
+void styles_build_msgbox(lv_obj_t *mbox)
+{
+    if (!mbox) {
+        return;
+    }
+    lv_obj_set_style_bg_color(mbox, UI_COLOR_CARD, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(mbox, LV_OPA_COVER, LV_PART_MAIN);
+    lv_obj_set_style_border_color(mbox, UI_COLOR_BORDER, LV_PART_MAIN);
+    lv_obj_set_style_border_width(mbox, 1, LV_PART_MAIN);
+    lv_obj_set_style_text_color(mbox, UI_COLOR_TEXT, LV_PART_MAIN);
+    lv_obj_set_style_text_color(mbox, UI_COLOR_TEXT, LV_PART_ITEMS);
+}
+
+void styles_build_keyboard(lv_obj_t *kbd)
+{
+    if (!kbd) {
+        return;
+    }
+    lv_obj_set_style_bg_color(kbd, UI_COLOR_CARD, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(kbd, LV_OPA_COVER, LV_PART_MAIN);
+    lv_obj_set_style_border_color(kbd, UI_COLOR_BORDER, LV_PART_MAIN);
+    lv_obj_set_style_border_width(kbd, 1, LV_PART_MAIN);
+    lv_obj_set_style_radius(kbd, 6, LV_PART_MAIN);
+    lv_obj_set_style_text_color(kbd, UI_COLOR_TEXT, LV_PART_MAIN);
+
+    /* Keys */
+    lv_obj_set_style_bg_color(kbd, UI_COLOR_CARD, LV_PART_ITEMS);
+    lv_obj_set_style_bg_opa(kbd, LV_OPA_COVER, LV_PART_ITEMS);
+    lv_obj_set_style_border_color(kbd, UI_COLOR_BORDER, LV_PART_ITEMS);
+    lv_obj_set_style_border_width(kbd, 1, LV_PART_ITEMS);
+    lv_obj_set_style_text_color(kbd, UI_COLOR_TEXT, LV_PART_ITEMS);
+
+    /* Pressed/checked/focused keys: subtle dark instead of accent */
+    lv_obj_set_style_bg_color(kbd, UI_COLOR_BORDER, LV_PART_ITEMS | LV_STATE_PRESSED);
+    lv_obj_set_style_bg_color(kbd, UI_COLOR_BORDER, LV_PART_ITEMS | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(kbd, UI_COLOR_BORDER, LV_PART_ITEMS | LV_STATE_FOCUSED);
+    lv_obj_set_style_bg_opa(kbd, LV_OPA_COVER, LV_PART_ITEMS | LV_STATE_PRESSED);
+    lv_obj_set_style_bg_opa(kbd, LV_OPA_COVER, LV_PART_ITEMS | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_opa(kbd, LV_OPA_COVER, LV_PART_ITEMS | LV_STATE_FOCUSED);
+    lv_obj_set_style_text_color(kbd, UI_COLOR_TEXT, LV_PART_ITEMS | LV_STATE_PRESSED);
+    lv_obj_set_style_text_color(kbd, UI_COLOR_TEXT, LV_PART_ITEMS | LV_STATE_CHECKED);
+    lv_obj_set_style_text_color(kbd, UI_COLOR_TEXT, LV_PART_ITEMS | LV_STATE_FOCUSED);
+}
